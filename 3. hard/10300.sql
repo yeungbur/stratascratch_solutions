0cdd231eaf707d50106cb2cd1a470183a7cf8f41
yeungbur <63782705+yeungbur@users.noreply.github.com>
2022-01-28 00:28:01 -0500

    Add solution

---
+++ 3. hard/10300.sql
@@ -0,0 +1,44 @@
+/*
+
+https://platform.stratascratch.com/coding/10300-premium-vs-freemium?
+
+
+Premium vs Freemium
+
+Find the total number of downloads for paying and non-paying users by date. Include only records where non-paying customers have more downloads than paying customers. The output should be sorted by earliest date first and contain 3 columns date, non-paying downloads, paying downloads.
+
+Tables: ms_user_dimension, ms_acc_dimension, ms_download_facts
+
+ms_user_dimension
+user_id int
+acc_id int
+
+ms_acc_dimension
+acc_id int
+paying_customer varchar
+
+ms_download_facts
+date datetime
+user_id int
+downloads int
+
+*/
+
+WITH A AS (
+SELECT *
+FROM ms_download_facts a
+JOIN ms_user_dimension b ON a.user_id = b.user_id
+JOIN ms_acc_dimension c ON b.acc_id = c.acc_id
+ORDER BY 1),
+
+B AS (
+SELECT date,
+SUM(CASE WHEN paying_customer = 'no' THEN downloads END) AS dl_np,
+SUM(CASE WHEN paying_customer = 'yes' THEN downloads END) AS dl_p
+FROM A
+GROUP BY date)
+
+SELECT *
+FROM B
+WHERE dl_np > dl_p
+ORDER BY date;
