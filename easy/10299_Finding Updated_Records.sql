67a26ce652c289631216957298590bb385c1e63d
yeungbur <63782705+yeungbur@users.noreply.github.com>
2022-01-29 01:19:43 -0500

    Update index for Easy problems

--- easy/10299_Finding Updated_Records.sql
+++ easy/10299_Finding Updated_Records.sql
@@ -1,6 +1,7 @@
 /*
 
 https://platform.stratascratch.com/coding/10299-finding-updated-records?
+
 Finding Updated Records
 
 We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
